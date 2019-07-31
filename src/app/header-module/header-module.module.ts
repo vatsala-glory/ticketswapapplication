54b52ee5e80733c817214d4174e5c8ef907d160d
Agrawal <vatsala.glory@gmail.com>
2019-07-31 10:46:45 +0200

    added bootstrap

--- src/app/header-module/header-module.module.ts
+++ src/app/header-module/header-module.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponentComponent } from './header-component/header-component.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NgbModule
   ],
   exports: [HeaderComponentComponent],
   declarations: [HeaderComponentComponent, SearchBarComponent]
--- src/app/header-module/search-bar/search-bar.component.ts
+++ src/app/header-module/search-bar/search-bar.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.css']
+  styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent implements OnInit {
 
