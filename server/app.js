fcdd1427b73f65024415670fcb95b48c27a5e038
Agrawal <vatsala.glory@gmail.com>
2019-09-04 15:24:57 +0200

    added sample json

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var users = require('./routes/users');
+var music = require('./routes/music');
 
 var app = express();
 
@@ -11,6 +11,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
 
-app.use('/api/v1/users', users);
+app.use('/api/v1/music', music);
 
 module.exports = app;
--- server/routes/music.js
+++ server/routes/music.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const data = require('../dataSet/music.json');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.json({users: [{name: 'Timmy'}]});
+  res.header("Content-Type",'application/json');
+  res.json(data);
 });
 
 module.exports = router;
