68488222f966dc965f6a89a2dc26d19f509d6c31
Vatsala Agrawal <vatsala.glory@gmail.com>
2020-01-24 11:41:45 +0100

    readded effect

---
+++ src/app/ticket-result/data.effect.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Action, Store, select } from '@ngrx/store';
+import { Observable, of } from 'rxjs';
+import { catchError, map, mergeMap } from 'rxjs/operators';
+import * as EventActions from './event.action';
+import { Event}  from './event.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataEffect {
+
+  private url = '/api/v1/'
+
+  constructor( private http: HttpClient, private action$: Actions, private store: Store<any> ) {
+    this.state$ = this.store.select('state').subscribe(x => {
+      this.stateVariable = x;
+    });
+   }
+    state$:any;
+    stateVariable;  
+   
+
+   private get state() {
+    return this.stateVariable;
+   }
+  
+  
+
+  fetchData: Observable<Action>  = createEffect(() =>   
+  this.action$.pipe(
+    ofType(EventActions.BeginGetEventAction),
+    mergeMap(action =>
+      this.http.get(this.url + this.state).pipe(
+        map((data: Event[]) => {
+          return EventActions.SuccessGetEventAction({ payload: data });
+        }),
+        catchError((error: Error) => {
+          return of(EventActions.ErrorEventAction(error));
+        })
+      )
+    )
+  )
+    
+  );
+}
